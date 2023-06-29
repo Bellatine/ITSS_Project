0ca52b39fe7005876b198d09b416466e7946af94
namhhbg0001 <nam.ng205103@sis.hust.edu.vn>
2023-06-29 12:04:49 +0700

    update login

--- ITSS/src/main/java/project/itss/group11/itss/controller/LoginController.java
+++ ITSS/src/main/java/project/itss/group11/itss/controller/LoginController.java
@@ -2,9 +2,12 @@ package project.itss.group11.itss.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,6 +31,9 @@ public class LoginController extends BaseController implements Initializable{
     private static LoginService loginService = new LoginServiceImpl();
 
     @FXML
+    private Label status;
+
+    @FXML
     private PasswordField passwordField;
 
     @FXML
@@ -40,6 +46,13 @@ public class LoginController extends BaseController implements Initializable{
         // Thiết lập giá trị ban đầu
         //System.out.println("Load Oke");
         Constant.pool = ConnectionPool.getInstance("etc/database.config");
+        Text text = new Text("Tài khoản hoặc mật khẩu sai" );
+        text.setFill(Color.RED);
+        status.setGraphic(text);
+        status.setVisible(false);
+
+
+
         //logger.info("Start load all conf");
     }
 
@@ -51,25 +64,36 @@ public class LoginController extends BaseController implements Initializable{
         String password = passwordField.getText();
 
         logger.info("Start login user: " + username + " , pass: " + password);
-        if(loginService.checkLogin(username,password)) {
-            logger.info("Login Success");
-            Constant.employee = loginService.getUserInfor(Integer.parseInt(username));
-            if(Constant.employee.getRole()==3) {
-                //changeScene("staff/UserTemplate.fxml");
-            	EmployeeView employeeView = new EmployeeView(HelloApplication.MAIN_STAGE);
-            	employeeView.show();
-            }else if(Constant.employee.getRole()==1) {
-            	QLNSView qlnsView = new QLNSView(HelloApplication.MAIN_STAGE);
-            	qlnsView.show();
-            }else if(Constant.employee.getRole()==2){
-                ManagerView managerView = new ManagerView(HelloApplication.MAIN_STAGE);
-                managerView.show();
+        try {
+            if (loginService.checkLogin(username, password)) {
+                logger.info("Login Success");
+                Constant.employee = loginService.getUserInfor(Integer.parseInt(username));
+                if (Constant.employee.getRole() == 3) {
+                    //changeScene("staff/UserTemplate.fxml");
+                    EmployeeView employeeView = new EmployeeView(HelloApplication.MAIN_STAGE);
+                    employeeView.show();
+                } else if (Constant.employee.getRole() == 1) {
+                    QLNSView qlnsView = new QLNSView(HelloApplication.MAIN_STAGE);
+                    qlnsView.show();
+                } else if (Constant.employee.getRole() == 2) {
+                    ManagerView managerView = new ManagerView(HelloApplication.MAIN_STAGE);
+                    managerView.show();
+                }
+            } else {
+                loginFail();
             }
+        }catch (Exception e){
+            logger.error("Error in handleLogin ",e);
+            loginFail();
         }
-        else logger.info("Login Fail");
 
         // Xử lý logic đăng nhập ở đây
         // ...
+    }
+    private void loginFail(){
+        logger.info("Login Fail");
+        status.setVisible(true);
+        passwordField.setText(null);
     }
 
 }
